47a45234cfbb1c04494fefa72ef357bb3eb78bb8
yeah1tsmark <marcwafula635@gmail.com>
2023-03-15 09:14:47 +0300

    datatypes in kotlin

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,18 @@
+fun main() {
+    //Int
+    var age = 20
+    var agee:Int = 20
+    //Strings
+    var name = "dj joe"
+    var namee:String = "dj joe"
+    //Chars
+    var city = 'K'
+    //Booleans
+    var status = true
+    var statuss = false
+    //Double
+    var temp = 36.5546354364
+    //Float
+    var lat:Float = 36.7F
+
+}
