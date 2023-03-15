ff5a730e59872589e5a447ae14c402cc6c7b4fb2
yeah1tsmark <marcwafula635@gmail.com>
2023-03-15 10:34:04 +0300

    exercise 1 on comparisons

---
+++ src/main/kotlin/ex-compare.kt
@@ -0,0 +1,60 @@
+fun main() {
+    //build a simple programme
+    // *544#
+    println("Enter Option")
+    var number:Int = readLine()!!.toInt()
+    if (number == 0) {
+        print("Sh20=1GB for 1hr")
+    }
+
+    else if (number == 1) {
+        print("Data Deals")
+    }
+
+    else if (number == 2) {
+        print("Daily Bundles")
+    }
+
+    else if (number == 3) {
+        print("Weekly Bundles")
+    }
+
+    else if (number == 4) {
+        print("GO MONTHLY")
+    }
+
+    else if (number == 5) {
+        print("No Expiry")
+    }
+
+    else if (number == 6) {
+        print("Video Bundles")
+    }
+
+    else if (number == 7) {
+        print("Okoa Data")
+    }
+
+    else if (number == 8) {
+        print("Lipa Mdogo")
+    }
+
+    else if (number == 9) {
+        print("Data Plus NEW")
+    }
+
+    else if (number == 10) {
+        print("Hot Minutes")
+    }
+
+    else if (number == 98) {
+        print("MORE")
+    }
+
+    else{
+        print("Invalid Choice. try Again")
+
+    }
+
+
+}
